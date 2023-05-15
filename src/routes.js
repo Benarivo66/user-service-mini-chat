3aa7006be9c5b42be8981b4dd8d15f0cf72db5c7
benarivo66 <iz4comed@yahoo.com>
2023-05-15 19:31:30 +0100

    add home route

--- src/routes.js
+++ src/routes.js
@@ -5,5 +5,6 @@ const { signup, login } = require("./controller");
 
 router.post("/signup", signup);
 router.post("/login", login);
+router.get("/", (req, res) => res.send("Welcome to User Mini Chat Service"));
 
 module.exports = router;
